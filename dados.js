39fa0813306576609cebacd77a1a497abcfe4a17
Pedro <pedro_schunck@outlook.com.br>
2024-09-07 13:33:07 -0300

    atualizando

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+    titulo: "Rayssa Leal",
+    descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens ",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira de destaque, conhecida por sua técnica e força. Ela conquistou diversas medalhas em competições nacionais e internacionais, sendo um dos principais nomes do judô brasileiro.",
+      link: "https://exemplo.com/beatriz-souza" // Substitua pelo link correto da Beatriz Souza
+    },
+    {
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com sua força e agilidade. Bicampeã olímpica, ela é um exemplo de superação e inspiração para muitos.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    }
+];
